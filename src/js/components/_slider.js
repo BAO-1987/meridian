806606188a6677749ba7ce76b87d98f70e7f4228
BAO <andriy.bezkorovajnyj@gmail.com>
2023-02-20 16:36:20 +0200

    inspired

--- src/js/components/_slider.js
+++ src/js/components/_slider.js
@@ -1,8 +1,9 @@
 import Swiper, {
   Navigation,
+  Pagination,
 } from 'swiper/bundle';
 
-const classSlider = new Swiper(".shopping-slider", {
+const shoppingSlider = new Swiper(".shopping-slider", {
   slidesPerView: 4,
   spaceBetween: 20,
   mousewheelControl: true,
@@ -27,3 +28,35 @@ const classSlider = new Swiper(".shopping-slider", {
     prevEl: ".shopping-slider__prev",
   },
 });
+
+const reviewsSlider = new Swiper(".reviews-slider", {
+  slidesPerView: 1,
+  mousewheelControl: true,
+  keyboardControl: true,
+  grabCursor: true,
+  // loop: true,
+  // centeredSlides: true,
+
+  // autoplay: {
+  //   delay: 2000,
+  //   disableOnInteraction: false,
+  // },
+
+
+  // a11y: {
+  //   prevSlideMessage: 'Попередній слайд',
+  //   nextSlideMessage: 'Наступний слайд',
+  // },
+
+  // navigation: {
+  //   nextEl: ".shopping-slider__next",
+  //   prevEl: ".shopping-slider__prev",
+  // },
+
+  pagination: {
+    el: '.reviews-slider__pagination',
+    type: 'fraction',
+    clickable: true,
+  },
+});
+
